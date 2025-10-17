8421ef2881e1f38014fb2eb45476ea79caa1b4c8
tanisha <161014421+tanisha0423@users.noreply.github.com>
2025-10-17 11:06:47 +0530

    Update Supabase client configuration with new URL and key

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "supabaseUrl";
-const supabaseAnonKey = "supabaseAnonKey";
+const supabaseUrl = "https://naupxtislsuwarnvxzax.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hdXB4dGlzbHN1d2FybnZ4emF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MDAwODIsImV4cCI6MjA3NTQ3NjA4Mn0.Swjnu1by9OBJUSY57FAcbMqFk-b5uAtgfJTfy7AvxG0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
